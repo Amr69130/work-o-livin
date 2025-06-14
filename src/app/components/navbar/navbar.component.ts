cc711e3b1fb9555708e2492570a6d49549404f51
Amr69130 <amrouchenorris@gmail.com>
2025-06-14 15:18:22 +0200

    add navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  darkMode = false;
 
+  constructor() {
+    // Vérifie la préférence stockée au démarrage
+    this.darkMode = localStorage.getItem('darkMode') === 'true';
+    if (this.darkMode) {
+      document.body.classList.add('dark');
+    }
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark');
+    localStorage.setItem('darkMode', this.darkMode.toString());
+  }
 }
+
